90469d5fbb14c819b34957d729445c26e24136d9
Carlos <carloselima2001@gmail.com>
2024-06-12 16:26:28 -0300

    Initial project setup

---
+++ Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAuth
+{
+    public class Settings
+    {
+        public static string Secret = "5KJvsngHeMpm884wtkJNzQGaCErckhHJBGFsvd3VyK5qMZXj3hS";
+    }
+}
