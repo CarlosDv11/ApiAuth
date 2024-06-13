05a0374ffaf62f041057c199ec2f08c4b254e9f7
Carlos <carloselima2001@gmail.com>
2024-06-13 11:39:44 -0300

    Add token generation functionality

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using ApiAuth.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ApiAuth.Repositories
-{
-    public class UserRepository
     {
+        public class UserRepository
+        {
+            public static User Get(string username, string password)
+            {
+                var users = new List<User>();
+                users.Add(new User { Id = 1, Username = "adm", Password = "123" });
+                users.Add(new User { Id = 2, Username = "user", Password = "123" });
+                return users.FirstOrDefault(x => x.Username.ToLower() == username.ToLower() && x.Password == password);
+
+            }
+        }
     }
-}
+
+
